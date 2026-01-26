a50ebbbee582a87c2ee84ac0e3166b5ed33078b1
3204roboticsWHS <aarav_shah@myabcusd.org>
2026-01-25 20:53:00 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function targetCalendar(){
+    window.open("https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=NzdncmptY29tYW80NHIxa3FxaWxlbjdnbWIgYWFyYXZfc2hhaEBteWFiY3VzZC5vcmc&amp;tmsrc=aarav_shah%40myabcusd.org", "_blank");
+}
